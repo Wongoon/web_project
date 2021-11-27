4bc982a53d8c18e80e519e1720e1a0f9ba090bc0
Wongoon <rmsgh64@gmail.com>
2021-11-27 21:27:42 +0900

    code update

--- javascript/index.js
+++ javascript/index.js
@@ -4,4 +4,14 @@ jQuery(document).ready(function(){
         $('.image_slide span:first-child').fadeOut('slow')
         .next('span').fadeIn('slow')
         .end().appendTo('.image_slide');}, 7000);
+
+    $('ul.exotic_weapon_ul li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+        
+        $('ul.exotic_weapon_ul li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    });
 });
