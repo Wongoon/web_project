55193dcff17d87cc7c097136b178ad5c59d1b525
Wongoon <rmsgh64@gmail.com>
2021-11-30 23:00:35 +0900

    code update

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,4 @@
 jQuery(document).ready(function(){
-    $('.image_slide span:gt(0)').hide();
-    setInterval(function(){
-        $('.image_slide span:first-child').fadeOut('slow')
-        .next('span').fadeIn('slow')
-        .end().appendTo('.image_slide');}, 7000);
-
     $('ul.tabnav_ul li').click(function(){
         var tab_id = $(this).attr('data-tab');
         
--- javascript/main.js
+++ javascript/main.js
@@ -4,4 +4,10 @@ jQuery(document).ready(function(){
     }).mouseout(function(){
         $(this).find('.nav-in').stop().slideUp(500);
     });
+    
+    $('.image_slide span:gt(0)').hide();
+    setInterval(function(){
+        $('.image_slide span:first-child').fadeOut('slow')
+        .next('span').fadeIn('slow')
+        .end().appendTo('.image_slide');}, 7000);
 });
